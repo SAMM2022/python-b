80a70eda1504334fc1c90d2c14a5bb5910c29654
SAMM2022 <SamMoeiny@gmail.com>
2022-11-06 13:36:40 -0500

    from locall

--- 1st.py
+++ 1st.py
@@ -1,10 +1,7 @@
 common i am remote
-<<<<<<< HEAD
 let see how to fix the bug here
-=======
 I need some other log for test
->>>>>>> 0676c4ac9c4b66db25de05ed0dd58c37e0df42f6
-#this is new line here remotely
+#this is new line here remotely and locally
 #this is the new python
 # it almost ready to merge to master branch
 username = input("What's your name?")
