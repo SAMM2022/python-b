db02cdce4a932ef0470fb3580ca2010928f6843c
SAMM2022 <102154517+SAMM2022@users.noreply.github.com>
2022-11-06 13:35:34 -0500

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -4,7 +4,7 @@ let see how to fix the bug here
 =======
 I need some other log for test
 >>>>>>> 0676c4ac9c4b66db25de05ed0dd58c37e0df42f6
-#this is new line here
+#this is new line here remotely
 #this is the new python
 # it almost ready to merge to master branch
 username = input("What's your name?")
